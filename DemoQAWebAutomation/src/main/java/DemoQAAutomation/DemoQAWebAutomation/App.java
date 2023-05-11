176622257d9123391bd639b3bb61185b615394a6
ajith <ajith.ssk@gmail.com>
2023-05-11 12:22:20 +0530

    Basemethod added

---
+++ DemoQAWebAutomation/src/main/java/DemoQAAutomation/DemoQAWebAutomation/App.java
@@ -0,0 +1,13 @@
+package DemoQAAutomation.DemoQAWebAutomation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
